86a4183f7516da6e9d9e28a73a4668234eed2864
zi-zzang <kjhwlgusdl@gmail.com>
2023-09-08 01:02:08 +0900

    9/8

--- src/pages/FindAccount.js
+++ src/pages/FindAccount.js
@@ -1,12 +1,22 @@
 import React from "react";
 import Header from "../components/common/Header";
-
+import styled from "styled-components";
+import FindAccountBox from "../components/auth/FindAccountBox";
+import { Align } from "../styled/ProjectStyle";
 const FindAccount = () => {
     return (
         <div>
-            <Header/>            
+            <Header/>
+            <Box>
+                <FindAccountBox/>    
+            </Box>
         </div>
     );
 };
+
+const Box = styled.div`
+    margin:30px 160px;
+    border: 1px solid #e2e8ff;
+`
 
 export default FindAccount;
--- src/pages/ResetPassword.js
+++ src/pages/ResetPassword.js
@@ -1,12 +1,20 @@
 import React from "react";
 import Header from "../components/common/Header";
-
+import ResetPwBox from "../components/auth/ResetPwBox";
+import styled from "styled-components";
 const ResetPassword = () => {
     return (
         <div>
-            <Header/>            
+            <Header/>
+            <Box>
+                <ResetPwBox/>    
+            </Box>
         </div>
     );
 };
+
+const Box = styled.div`
+    margin:30px 160px;
+`
 
 export default ResetPassword;
